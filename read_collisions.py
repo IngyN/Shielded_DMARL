7ebbbdbbee13d1b6b2ea084ab23b2a914c184870
IngyN <ingyn@noreply.github.com>
2020-07-15 14:02:10 -0400

    updated collisions file to include csv output for learning curves

--- read_collisions.py
+++ read_collisions.py
@@ -1,9 +1,15 @@
 import pickle
 import numpy as np
+import csv
 
-file = 'benchmark_files/debug_collisions.pkl'
+to_csv = True
 
-with open(file, 'rb') as f:
+coll_dir = 'benchmark_files/'
+rew_dir= 'learning_curves/'
+exp_name = 'debug_5_agents_no_shield'
+
+coll_file = coll_dir + exp_name + '.pkl'
+with open(coll_file, 'rb') as f:
     c = pickle.load(f, encoding='bytes')
 
 sum = 0
@@ -12,4 +18,17 @@ for i in c:
     for j in i:
         sum+= j
 
-print(f'Collisions sum {sum}')
+print(f' File : {coll_file}, Collisions sum {sum/2}')
+
+if to_csv:
+    rew_file = rew_dir + exp_name +'_rewards.pkl'
+    new_file = rew_dir + exp_name + '_rewards.csv'
+
+    with open(rew_file, 'rb') as f:
+        rew = pickle.load(f, encoding='bytes')
+
+    print(f' New file :{new_file}')
+
+    with open(new_file, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter='\n')
+        writer.writerow(rew)
